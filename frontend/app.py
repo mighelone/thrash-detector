36076d84bb8eb3156eefe991ad4b0e5515259cf4
Michele Vascellari <michele.vascellari@gmail.com>
2020-05-09 13:45:44 +0100

    Modify frontend for showing results

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
-app = Flask(__name__)
+BASE_PATH = os.path.dirname(__file__)
+app = Flask(__name__, static_folder=os.path.join(BASE_PATH, "static"))
 
 app.config.from_object(os.environ.get("FLASK_CONFIG", "frontend.config.TestConfig"))
 
@@ -13,18 +14,22 @@ headers = {"content-type": content_type}
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
-def hello():
-    print(request)
+def index():
     if request.method == "POST":
         if request.files:
 
-            img1 = request.files["image-1"]
-            img2 = request.files["image-2"]
             files = {
-                "image1": request.files["image-1"],
-                "image2": request.files["image-2"],
+                "image1": request.files["image-1"].read(),
+                "image2": request.files["image-2"].read(),
             }
-            response = requests.post("{}/post".format(app.config["BACKEND_URL"]), files=files)
-            return render_template("index.html", comment="Done!")
+            response = requests.post("{}/api".format(app.config["BACKEND_URL"]), files=files)
+            with open(os.path.join(app.static_folder, "upload/result.jpg"), "wb") as f:
+                f.write(response.content)
+            return render_template("index.html", filename="result.jpg")
 
     return render_template("index.html")
+
+
+@app.route("/display/<filename>")
+def display_image(filename):
+    return redirect(url_for("static", filename=os.path.join("upload/", filename)), code=301)
